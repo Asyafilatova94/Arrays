66c6628b8612b5209725a2df28caf3a238c39972
Anastasiya Voronina <Asyafilatova@94gmail.com>
2023-12-13 22:20:31 +0300

    Домашнее задание.

---
+++ Class_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] array = new int[10] { 45, 26, 63, 68, 57, 34, 55, 56, 86, 26 };
+int even_numbers = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    var reminderDivision = array[i] % 2; // остаток от деление текущего элемента массива на 2
+
+    if (reminderDivision == 0) // если остаток = 0 это четное число
+    {
+        even_numbers = even_numbers + 1;  //even_numbers++; или
+    }
+}
+
+Console.WriteLine($"Количество {even_numbers}");
---
+++ Class_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+var array = new[] { 3.4, 45.6, 34.5, 21.2, 65.2, 1.4, 182.2, 4.6 };
+
+var minIndex = 0;
+var maxIndex = 0;
+
+for (var i = 0; i < array.Length; i++)
+{
+    if(array[minIndex] > array[i])
+    {
+        minIndex = i;
+    }
+
+    if(array[maxIndex] < array[i])
+    {
+        maxIndex = i;
+    }
+}
+
+Console.WriteLine($"Минимальный элемент - {array[minIndex]}");
+Console.WriteLine($"Максимальный элемент - {array[maxIndex]}");
+var diff = array[maxIndex] - array[minIndex];
+Console.WriteLine($"Разница между максимальным и минимальным элементом - {diff}");
+
